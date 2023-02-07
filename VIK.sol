aec6f894431eddcd7896a731fbb33014a6155786
Viktor <80635831+SOFZP@users.noreply.github.com>
2023-02-08 00:14:43 +0200

    Add files via upload

--- VIK.sol
+++ VIK.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "ERC20.sol";
-import "ERC20Burnable.sol";
-import "Ownable.sol";
-import "draft-ERC20Permit.sol";
-import "ERC20Votes.sol";
+import "./@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "./@openzeppelin/contracts/access/Ownable.sol";
+import "./@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "./@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract VIK is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
     constructor() ERC20("VIK", "VIK") ERC20Permit("VIK") {
